9d99ffe8ad7a6339c206285c797dd37573c520b4
Ziheng Fang <1604833664@qq.com>
2023-03-31 23:27:35 +0800

    C语言练习

--- 计算1!+2!+3!+····n!/计算1!+2!+····n!.cpp
+++ 计算1!+2!+3!+····n!/计算1!+2!+····n!.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
-
 int main()
 {
 	int n = 0, i = 0;
 	int factorial = 1;
-	printf("请输入 n = ");
+	int sum = 0;
+	printf("请输入 n:");
 	scanf_s("%d", &n);
 	for (i = 1; i <= n; i++)
 	{
 		factorial *= i;
+		sum += factorial;
 	}
-	printf("%d的阶乘为%d\n", n, factorial);
+	printf("%d", sum);
 }
